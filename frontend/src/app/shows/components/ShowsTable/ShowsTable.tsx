6afd4131132c1f0b5b7aab994813eb33d4f23031
Logan Underwood <lunderwood@vailsys.com>
2024-06-27 23:34:01 -0500

    Add nvmrc file

--- frontend/src/app/shows/components/ShowsTable/ShowsTable.tsx
+++ frontend/src/app/shows/components/ShowsTable/ShowsTable.tsx
@@ -11,13 +11,13 @@ export interface ShowsTableProps {
 export const ShowsTable: FunctionComponent<ShowsTableProps> = ({ shows }) => {
   return(
     <>
-    <table className="flex flex-col justify-start w-full h-full mb-auto">
-      <tr className="flex flex-row justify-start pl-6 pr-6 items-center h-20">
-        <th className="flex justify-start w-1/4">Location</th>
-        <th>Date</th>
-      </tr>
-      {shows.map((show, index) => <ShowsTableRow key={index} show={show}/>)}
-    </table>
+      <table className="flex flex-col justify-start w-full h-full mb-auto">
+        <tr className="flex flex-row justify-start pl-6 pr-6 items-center h-20">
+          <th className="flex justify-start w-1/4">Location</th>
+          <th>Date</th>
+        </tr>
+        {shows.map((show, index) => <ShowsTableRow key={index} show={show}/>)}
+      </table>
     </>
   )
 }
